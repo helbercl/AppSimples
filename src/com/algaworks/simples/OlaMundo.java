023582edf4d876799684b72ec4b8788e788d67f8
Helber Cardoso Lemos <helber@helber-Login>
2018-12-18 17:18:04 -0300

    initial commit

---
+++ src/com/algaworks/simples/OlaMundo.java
@@ -0,0 +1,37 @@
+package com.algaworks.simples;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/OlaMundo")
+public class OlaMundo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		PrintWriter	out = response.getWriter();	// response retorna /devolve
+		out.println("<html>");
+		   out.println("<head>");
+		   out.println("</head>");
+		   out.println("<body>");
+		      out.println("<h1>Ola Mundo</h1>"); 
+		      out.println("<p> Testando Servlet no primeiro projeto</p>");
+		   out.println("</body>");
+		out.println("</html>");
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
